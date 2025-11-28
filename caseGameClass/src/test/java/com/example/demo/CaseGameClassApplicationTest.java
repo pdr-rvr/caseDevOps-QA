16452c3034b3e4230c710459a976860ac2801fdf
pdr-rvr <144543133+pdr-rvr@users.noreply.github.com>
2025-11-28 20:26:22 -0300

    Update CaseGameClassApplicationTest.java

--- caseGameClass/src/test/java/com/example/demo/CaseGameClassApplicationTest.java
+++ caseGameClass/src/test/java/com/example/demo/CaseGameClassApplicationTest.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class CaseGameClassApplicationTest {
-
+    
     @MockBean
     private ConnectionFactory connectionFactory;
 
