9093ca398fa58b1070e83a507764d3e50fcfc90e
pdr-rvr <144543133+pdr-rvr@users.noreply.github.com>
2025-11-28 20:22:58 -0300

    Update CaseGameClassApplicationTest.java

--- caseGameClass/src/test/java/com/example/demo/CaseGameClassApplicationTest.java
+++ caseGameClass/src/test/java/com/example/demo/CaseGameClassApplicationTest.java
@@ -2,10 +2,15 @@ package com.example.demo;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class CaseGameClassApplicationTest {
+
+    @MockBean
+    private ConnectionFactory connectionFactory;
 
     @Test
     @DisplayName("Deve carregar o contexto da aplicação")
